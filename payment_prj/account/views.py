032d21faffbf86b6b49c4c60adecc03c413a823b
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-07-15 21:35:49 +0200

    homepage
    added proper homepage template, both text and static elements load properly

--- payment_prj/account/views.py
+++ payment_prj/account/views.py
@@ -6,27 +6,28 @@ from django.contrib import messages
 # Create your views here.
 
 def kyc_registration(request):
-    user=request.user
-    account=Account.object.get(user=user)
+    user = request.user
+    account = Account.objects.get(user=user)
 
     try:
-        kyc=KYC.obj.get(user=user)
-    except:
-        kyc=None
+        kyc = KYC.objects.get(user=user)
+    except KYC.DoesNotExist:
+        kyc = None
 
-    if request.method=="POST":
-        form = KYCForm(request.POST,request.FILES,instance=kyc)
+    if request.method == "POST":
+        form = KYCForm(request.POST, request.FILES, instance=kyc)
         if form.is_valid():
-            new_form=form.save(commit=False)
-            new_form.user=user
-            new_form.account=account
+            new_form = form.save(commit=False)
+            new_form.user = user
+            new_form.account = account
             new_form.save()
             messages.success(request, "KYC Form submitted successfully. Review pending.")
             return redirect("core:index")
     else:
-        form=KYCForm(instance=kyc)
-    context:{
-        "account":account,
-        "form":form,
+        form = KYCForm(instance=kyc)
+
+    context = {
+        "account": account,
+        "form": form,
     }
-    return render(request,"account/kyc-form.html",context)
+    return render(request, "account/kyc-form.html", context)
